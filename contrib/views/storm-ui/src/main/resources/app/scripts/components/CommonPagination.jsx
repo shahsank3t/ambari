10bee67ac3591469ffd992fb563c2ccf9fa0bc66
sameer79 <findsameershaikh@yahoo.co.in>
2017-07-12 14:44:04 +0530

    Add CommonPagination component

---
+++ contrib/views/storm-ui/src/main/resources/app/scripts/components/CommonPagination.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import _ from 'lodash';
+import {Pagination} from 'react-bootstrap';
+
+export default class CommonPagination extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  handleSelect = (eventKey) => {
+    this.props.callBackFunction(eventKey);
+  }
+
+  render(){
+    const {activePage,pageSize,filteredEntities} = this.props;
+    const totalPages = Math.ceil(filteredEntities.length / pageSize);
+
+    return(
+      <div className="pagination-wrapper">
+        <div className="pull-left">
+          <span>{`Showing ${activePage > 1 ? (activePage-1)*pageSize : activePage }  ot ${activePage*pageSize > filteredEntities.length ? filteredEntities.length : (activePage*pageSize)} of ${filteredEntities.length} entries`}</span>
+        </div>
+        <Pagination
+         className={`${filteredEntities.length === 0? 'hidden':'shown pull-right'}`}
+         prev
+         next
+         first
+         last
+         ellipsis
+         items={totalPages}
+         maxButtons={5}
+         activePage={activePage}
+         onSelect={this.handleSelect}>
+      </Pagination>
+      </div>
+    );
+  }
+}
