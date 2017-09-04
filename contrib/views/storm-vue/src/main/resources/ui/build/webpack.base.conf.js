6b628b05a06f7d8e9864f353906373f453bc0651
Sanket Shah <er.sanketshah@gmail.com>
2017-09-04 10:55:16 +0530

    fixed loading image path

--- contrib/views/storm-vue/src/main/resources/ui/build/webpack.base.conf.js
+++ contrib/views/storm-vue/src/main/resources/ui/build/webpack.base.conf.js
@@ -58,6 +58,7 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
+          publicPath: '../../',
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
